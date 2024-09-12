2ac9ee57fef36403692bcfef75fd83545daa4a6a
Ryan Torn <rtorn@albany.edu>
2024-09-12 18:32:37 +0000

    updating for new configure dictionary structure.  Simplifying to download control and perturbed members at the same time

--- tigge_download.py
+++ tigge_download.py
@@ -40,39 +40,39 @@ confin = configparser.ConfigParser()
 confin.read(paramfile)
 
 config = {}
-config.update(confin['model'])
-config.update(confin['locations'])
+config['model']     = confin['model']
+config['locations'] = confin['locations']
 
 #  Modify work and output directory for specific case/time
 if args.storm:
   storm = args.storm
-  config['work_dir']   = '{0}/{1}.{2}'.format(config['work_dir'],yyyymmddhh,storm)
+  config['locations']['work_dir']   = '{0}/{1}.{2}'.format(config['locations']['work_dir'],yyyymmddhh,storm)
 else:
-  config['work_dir']   = '{0}/{1}'.format(config['work_dir'],yyyymmddhh)
+  config['locations']['work_dir']   = '{0}/{1}'.format(config['locations']['work_dir'],yyyymmddhh)
 
 #  Create appropriate directories
-if not os.path.isdir(config['work_dir']):
+if not os.path.isdir(config['locations']['work_dir']):
    try:
-      os.makedirs(config['work_dir'])
+      os.makedirs(config['locations']['work_dir'])
    except OSError as e:
       raise e
 
 #  Set the TIGGE read command values
-if not config['tigge_forecast_time']:
+if not config['model']['tigge_forecast_time']:
    raise "tigge_forecast_time is missing"
 
 daystr = yyyymmddhh[0:4] + '-' + yyyymmddhh[4:6] + '-' + yyyymmddhh[6:8]
 hhstr  = yyyymmddhh[8:10] + ":00:00"
 
-if config['model_src'] == 'ECMWF':
+if config['model']['model_src'] == 'ECMWF':
   model_str = "ecmf"
-  model_num = "1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28/29/30/31/32/33/34/35/36/37/38/39/40/41/42/43/44/45/46/47/48/49/50"
-elif config['model_src'] == 'GEFS':
+  model_num = "0/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28/29/30/31/32/33/34/35/36/37/38/39/40/41/42/43/44/45/46/47/48/49/50"
+elif config['model']['model_src'] == 'GEFS':
   model_str = "kwbc"
-  model_num = "1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28/29/30"
+  model_num = "0/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28/29/30"
 #  model_num = "1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20"
 
-os.chdir(config['work_dir'])
+os.chdir(config['locations']['work_dir'])
 
 server = ECMWFDataServer()
 
@@ -85,29 +85,14 @@ server.retrieve({
     'parameter' : "130/131/132/133/156",
     'number'    : model_num,
     'dataset'   : "tigge",
-    'step'      : config['tigge_forecast_time'],
-    'grid'      : config.get('tigge_forecast_grid','1.0/1.0'),
+    'step'      : config['model']['tigge_forecast_time'],
+    'grid'      : config['model'].get('tigge_forecast_grid','1.0/1.0'),
     'time'      : hhstr,
-    'type'      : "pf",
+    'type'      : "pf/cf",
     'date'      : daystr,
     'class'     : "ti",
-    'target'    : "tigge_output_pl_pf.grib"
-})
-
-server.retrieve({
-    'origin'    : model_str,
-    'levelist'  : "200/250/300/500/700/850/925/1000",
-    'levtype'   : "pl",
-    'expver'    : "prod",
-    'parameter' : "130/131/132/133/156",
-    'dataset'   : "tigge",
-    'step'      : config['tigge_forecast_time'],
-    'grid'      : config.get('tigge_forecast_grid','1.0/1.0'),
-    'time'      : hhstr,
-    'type'      : "cf",
-    'date'      : daystr,
-    'class'     : "ti",
-    'target'    : "tigge_output_pl_cf.grib"
+    'packing'   : "simple",
+    'target'    : "tigge_output_pl.grib"
 })
 
 '''
@@ -120,8 +105,8 @@ server.retrieve({
     'parameter' : "3/131/132",
     'number'    : model_num,
     'dataset'   : "tigge",
-    'step'      : config['tigge_forecast_time'],
-    'grid'      : config.get('tigge_forecast_grid','1.0/1.0'),
+    'step'      : config['model']['tigge_forecast_time'],
+    'grid'      : config['model'].get('tigge_forecast_grid','1.0/1.0'),
     'time'      : hhstr,
     'type'      : "pf",
     'date'      : daystr,
@@ -136,8 +121,8 @@ server.retrieve({
     'expver'    : "prod",
     'parameter' : "3/131/132",
     'dataset'   : "tigge",
-    'step'      : config['tigge_forecast_time'],
-    'grid'      : config.get('tigge_forecast_grid','1.0/1.0'),
+    'step'      : config['model']['tigge_forecast_time'],
+    'grid'      : config['model'].get('tigge_forecast_grid','1.0/1.0'),
     'time'      : hhstr,
     'type'      : "cf",
     'date'      : daystr,
@@ -155,28 +140,14 @@ server.retrieve({
     'parameter' : "59/136/151/167/168",
     'number'    : model_num,
     'dataset'   : "tigge",
-    'step'      : config['tigge_forecast_time'],
-    'grid'      : config.get('tigge_forecast_grid','1.0/1.0'),
+    'step'      : config['model']['tigge_forecast_time'],
+    'grid'      : config['model'].get('tigge_forecast_grid','1.0/1.0'),
     'time'      : hhstr,
-    'type'      : "pf",
+    'type'      : "pf/cf",
     'date'      : daystr,
     'class'     : "ti",
-    'target'    : "tigge_output_sfc_pf.grib"
-})
-
-server.retrieve({
-    'origin'    : model_str,
-    'levtype'   : "sfc",
-    'expver'    : "prod",
-    'parameter' : "59/136/151/167/168",
-    'dataset'   : "tigge",
-    'step'      : config['tigge_forecast_time'],
-    'grid'      : config.get('tigge_forecast_grid','1.0/1.0'),
-    'time'      : hhstr,
-    'type'      : "cf",
-    'date'      : daystr,
-    'class'     : "ti",
-    'target'    : "tigge_output_sfc_cf.grib"
+    'packing'   : "simple",
+    'target'    : "tigge_output_sfc.grib"
 })
 
 #  Retrieve higher-resolution surface fields
@@ -187,38 +158,22 @@ server.retrieve({
     'parameter' : "165/166/228",
     'number'    : model_num,
     'dataset'   : "tigge",
-    'area'      : config.get('tigge_surface_area','90/-180/-90/179'),
-    'step'      : config.get('tigge_surface_time',config['tigge_forecast_time']),
-    'grid'      : config.get('tigge_surface_grid_space','0.25/0.25'),
+    'area'      : config['model'].get('tigge_surface_area','90/-180/-90/179'),
+    'step'      : config['model'].get('tigge_surface_time',config['model']['tigge_forecast_time']),
+    'grid'      : config['model'].get('tigge_surface_grid_space','0.25/0.25'),
     'time'      : hhstr,
-    'type'      : "pf",
+    'type'      : "pf/cf",
     'date'      : daystr,
     'class'     : "ti",
-    'target'    : "tigge_output_hrsfc_pf.grib"
-})
-
-server.retrieve({
-    'origin'    : model_str,
-    'levtype'   : "sfc",
-    'expver'    : "prod",
-    'parameter' : "165/166/228",
-    'dataset'   : "tigge",
-    'area'      : config.get('tigge_surface_area','90/-180/-90/179'),
-    'step'      : config.get('tigge_surface_time',config['tigge_forecast_time']),
-    'grid'      : config.get('tigge_surface_grid_space','0.25/0.25'),
-    'time'      : hhstr,
-    'type'      : "cf",
-    'date'      : daystr,
-    'class'     : "ti",
-    'target'    : "tigge_output_hrsfc_cf.grib"
+    'packing'   : "simple",
+    'target'    : "tigge_output_hrsfc.grib"
 })
 
 for ftype in ["pl", "sfc", 'hrsfc']:
-   os.system("wgrib2 -s tigge_output_{0}_pf.grib >& grib_{0}_pf.out".format(ftype))
-   os.system("wgrib2 -s tigge_output_{0}_cf.grib >& grib_{0}_cf.out".format(ftype))
+   os.system("wgrib2 -s tigge_output_{0}.grib >& grib_{0}.out".format(ftype))
 
-hlist = config['tigge_forecast_time'].split("/")
-slist = config.get('tigge_surface_time',config['tigge_forecast_time']).split("/")
+hlist = config['model']['tigge_forecast_time'].split("/")
+slist = config['model'].get('tigge_surface_time',config['model']['tigge_forecast_time']).split("/")
 
 init   = dt.datetime.strptime(yyyymmddhh, '%Y%m%d%H')
 init_s = init.strftime("%m%d%H%M")
@@ -248,17 +203,12 @@ for t in range(len(hlist)):
   gribout = 'E1E{0}{1}1'.format(str(init_s), str(datef_s))  
 
   for ftype in ["pl", "sfc"]:
-     os.system("cat grib_{0}_cf.out | grep \"{1}\" | wgrib2 -fix_ncep -i -append tigge_output_{0}_cf.grib \
+     os.system("cat grib_{0}.out | grep \"{1}\" | wgrib2 -fix_ncep -i -append tigge_output_{0}.grib \
                    -grib {2} >& /dev/null".format(ftype,timestr,gribout))
-     os.system("cat grib_{0}_pf.out | grep \"{1}\" | wgrib2 -fix_ncep -i -append tigge_output_{0}_pf.grib \
-                   -grib {2} >& /dev/null".format(ftype,timestr,gribout))
-
 
 for ftype in ["pl", "sfc"]:
-   os.remove("tigge_output_{0}_pf.grib".format(ftype))
-   os.remove("tigge_output_{0}_cf.grib".format(ftype))
-   os.remove("grib_{0}_pf.out".format(ftype))
-   os.remove("grib_{0}_cf.out".format(ftype))
+   os.remove("tigge_output_{0}.grib".format(ftype))
+   os.remove("grib_{0}.out".format(ftype))
 
 
 for t in range(len(slist)):
@@ -285,20 +235,14 @@ for t in range(len(slist)):
   gribout = 'E1E{0}{1}1'.format(str(init_s), str(datef_s))
 
   for ftype in ['hrsfc']:
-     os.system("cat grib_{0}_cf.out | grep \"{1}\" | wgrib2 -fix_ncep -i -append tigge_output_{0}_cf.grib \
-                   -grib {2} >& /dev/null".format(ftype,timestr,gribout))
-     os.system("cat grib_{0}_pf.out | grep \"{1}\" | wgrib2 -fix_ncep -i -append tigge_output_{0}_pf.grib \
+     os.system("cat grib_{0}.out | grep \"{1}\" | wgrib2 -fix_ncep -i -append tigge_output_{0}.grib \
                    -grib {2} >& /dev/null".format(ftype,timestr,gribout))
 
   if int(slist[t]) > 0:
-     os.system("cat grib_hrsfc_cf.out | grep \"{0}\" | wgrib2 -fix_ncep -i -append tigge_output_hrsfc_cf.grib \
-                   -grib {1} >& /dev/null".format(pcpstr,gribout))
-     os.system("cat grib_hrsfc_pf.out | grep \"{0}\" | wgrib2 -fix_ncep -i -append tigge_output_hrsfc_pf.grib \
+     os.system("cat grib_hrsfc.out | grep \"{0}\" | wgrib2 -fix_ncep -i -append tigge_output_hrsfc.grib \
                    -grib {1} >& /dev/null".format(pcpstr,gribout))
 
 
 for ftype in ["hrsfc"]:
-   os.remove("tigge_output_{0}_pf.grib".format(ftype))
-   os.remove("tigge_output_{0}_cf.grib".format(ftype))
-   os.remove("grib_{0}_pf.out".format(ftype))
-   os.remove("grib_{0}_cf.out".format(ftype))
+   os.remove("tigge_output_{0}.grib".format(ftype))
+   os.remove("grib_{0}.out".format(ftype))
